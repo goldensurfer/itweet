a744f68de58ea487229a37e9c7c6381a0f191b35
Paul Peter Flis <pawel@flycode.pl>
2013-07-30 20:26:28 +0200

    test timeout infinity

--- src/itweep.erl
+++ src/itweep.erl
@@ -87,7 +87,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 -define(INITIAL_BACKOFF, 10000).
--define(STREAM_TIMEOUT, 25000).
+-define(STREAM_TIMEOUT, infinity).
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% INTERNALS
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
